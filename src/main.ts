1acde71b812af079f5b1ddd4060a1a147f17c176
Joao Cunha <brlocky@gmail.com>
2023-12-07 20:56:52 +0000

    Swagger - Expose all DTOS

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { GetCandlesDto, GetCandlesResultDto, SearchDto, SearchResultDto } from './dto';
+import { CandleDto, CandlePointer, GetCandlesDto, GetCandlesResultDto, SearchDto, SearchResultDto } from './dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -13,8 +13,9 @@ async function bootstrap() {
     .addTag('TAG')
     .build();
   const document = SwaggerModule.createDocument(app, config, {
-    extraModels: [SearchDto, SearchResultDto, GetCandlesDto, GetCandlesResultDto],
+    extraModels: [SearchDto, SearchResultDto, GetCandlesDto, GetCandlesResultDto, CandlePointer, CandleDto],
   });
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
