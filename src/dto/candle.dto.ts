6f681198b65b20457176e25dd1f44bb1de474990
Joao Cunha <brlocky@gmail.com>
2023-12-07 20:48:58 +0000

    Swagger - Expose DTO Properties

--- src/dto/candle.dto.ts
+++ src/dto/candle.dto.ts
@@ -1,29 +1,49 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class GetCandlesDto {
+  @ApiProperty()
   symbol: string;
+  @ApiProperty()
   interval: string;
+  @ApiProperty()
   from?: string;
+  @ApiProperty()
   to?: string;
 }
 
 export class CandlePointer {
+  @ApiProperty()
   symbol: string;
+  @ApiProperty()
   interval: string;
+  @ApiProperty()
   time: string;
 }
 
 export class GetCandlesResultDto {
+  @ApiProperty()
   symbol: string;
+  @ApiProperty()
   interval: string;
+  @ApiProperty()
   candles: CandleDto[];
+  @ApiProperty()
   prevCandle: CandlePointer | null;
+  @ApiProperty()
   nextCandle: CandlePointer | null;
 }
 
 export class CandleDto {
+  @ApiProperty()
   time: number;
+  @ApiProperty()
   open: string;
+  @ApiProperty()
   high: string;
+  @ApiProperty()
   low: string;
+  @ApiProperty()
   close: string;
+  @ApiProperty()
   volume: string;
 }
--- src/dto/search.dto.ts
+++ src/dto/search.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SearchDto {
+  @ApiProperty()
   text: string;
 }
 
 export class SearchResultDto {
+  @ApiProperty()
   symbol: string;
+
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   type: string;
+
+  @ApiProperty()
   country: string;
 }
