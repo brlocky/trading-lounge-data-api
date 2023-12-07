638337f6d1c136abcabe36707171d360a9ecb753
Joao Cunha <brlocky@gmail.com>
2023-12-07 20:52:20 +0000

    Swagger - Expose DTO Properties

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GetCandlesDto, GetCandlesResultDto, SearchDto, SearchResultDto } from './dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -11,7 +12,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('TAG')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    extraModels: [SearchDto, SearchResultDto, GetCandlesDto, GetCandlesResultDto],
+  });
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
